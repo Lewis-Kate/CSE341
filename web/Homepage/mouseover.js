35be522721992a2003c824e2a29a8d56f5fca6f2
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-09-24 22:04:24 -0400

    final

--- web/Homepage/mouseover.js
+++ web/Homepage/mouseover.js
@@ -1,10 +1,10 @@
 
- function functionAlert(msg, myYes) {
-            var confirmBox = $("#confirm");
-            confirmBox.find(".message").text(msg);
-            confirmBox.find(".yes").unbind().click(function() {
-               confirmBox.hide();
-            });
-            confirmBox.find(".yes").click(myYes);
-            confirmBox.show();
-         }
+function functionAlert(msg, myYes) {
+    var confirmBox = $("#confirm");
+    confirmBox.find(".message").text(msg);
+    confirmBox.find(".yes").unbind().click(function () {
+        confirmBox.hide();
+    });
+    confirmBox.find(".yes").click(myYes);
+    confirmBox.show();
+}
