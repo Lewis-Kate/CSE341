c555373ea15eb13f4e27985c87c4695185536e36
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-10-16 00:01:50 -0400

    meals.php

---
+++ web/week05/meals.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Meals</title>
+    <meta name="Meals" content="Meals">
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&family=Rozha+One&display=swap" rel="stylesheet">
+</head>
+
+    
+<body>
+<div class="content">
+    <main>
+    
+    <div class="meals">    
+    <h1>Meals</h1>
+        <button><a href="index.php">Home</a></button>
+        <button><a href="index.php">Shopping List</a></button>
+        <button><a href="index.php">View Menu</a></button>
+    </div>
+    
+    </main>
+</div> 
+</body>
+
+
+
+
+
+</html>
