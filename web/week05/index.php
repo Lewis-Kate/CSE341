9a517470e27862c8490b7c9939a0df9459f16598
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-10-16 00:03:15 -0400

    index.php

--- web/week05/index.php
+++ web/week05/index.php
@@ -5,8 +5,24 @@
     <title>Menu Planner</title>
     <meta name="Menu Planner" content="Menu Planner Index">
     <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&family=Rozha+One&display=swap" rel="stylesheet">
 </head>
 
+    
+<body>
+<div class="content">
+    <main>
+    
+    <div class="home">    
+    <h1>Menu Planner</h1>
+        <button><a href="index.php">Plan Meals</a></button>
+        <button><a href="index.php">Shopping List</a></button>
+        <button><a href="index.php">View Menu</a></button>
+    </div>
+    
+    </main>
+</div> 
+</body>
 
 
 
