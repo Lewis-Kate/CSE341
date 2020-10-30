0f92220ac50ae3fb9048b2509e79040c16f9a51c
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-10-30 18:33:09 -0400

    open php

--- web/week05/delete.php
+++ web/week05/delete.php
@@ -2,43 +2,25 @@
 session_start();
 require "dbconnect.php";
 $db = get_db();
-?>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Menu Planner</title>
-    <meta name="Menu Planner" content="Menu Planner Index">
-    <link rel="stylesheet" href="main.css">
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&family=Rozha+One&display=swap" rel="stylesheet">
-</head>
 
-    <h1>Delete</h1>
-    
-
-    
-    
-    <?php
     
     switch ($action){
     case "mon":
             
-    $monday = filter_input(INPUT_POST, 'monday');     
+    $monday = "monday";     
     
-    $stmt = $db->prepare("DELETE FROM days WHERE monday = :monday");
+    $stmt = $db->prepare('DELETE FROM days WHERE monday = :monday');
     $stmt -> bindValue(':monday', $monday);
     
     
     $stmt->execute();
     $stmt->closeCursor();
-    header('Location: view.php');
-    
+    header('Location: view.php');    
    
     }
     ?>
 
 
 
-</html>
